ef287af3fbf0c881d41e53ddf2d4d74c83b0ea85
irisida <20445648+irisida@users.noreply.github.com>
2019-08-15 21:55:08 +0100

    added eact-router-dom, withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h2>Hats Page</h2>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+        {/* <Route path="/shop/jackets" component={JacketsPage} /> */}
+        {/* <Route path="/shop/sneakers" component={SneakersPage} /> */}
+      </Switch>
     </div>
   );
 }

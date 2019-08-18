62c6b6af158e4da0e11783207ff8bc3daf912133
irisida <20445648+irisida@users.noreply.github.com>
2019-08-18 22:08:48 +0100

    added sign out functionality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/prettygreen.svg';
+import { auth } from '../../firebase/firebase.utils';
 
-const Header = () => {
+import './header.styles.scss';
+
+const Header = ({ currentUser }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -16,6 +18,15 @@ const Header = () => {
         <Link className="option" to="/contact">
           Contact
         </Link>
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            Sign Out
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   );

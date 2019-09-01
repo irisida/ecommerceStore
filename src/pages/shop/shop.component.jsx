65625636904b6ace266a3933527e999922f8318d
irisida <20445648+irisida@users.noreply.github.com>
2019-09-01 22:21:14 +0100

    added changes to move in-component, in-app state to redux. Created default actions as the state data is pretty static

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-import SHOPDATA from './shop.data';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-class ShopPage extends React.Component {
-  state = {
-    collections: SHOPDATA,
-  };
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ collections }) => {
+  return (
+    <div className="shop-page">
+      <CollectionsOverview />
+    </div>
+  );
+};
 
 export default ShopPage;
